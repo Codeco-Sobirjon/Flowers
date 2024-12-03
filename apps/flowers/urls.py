69f372a95f00fd3ec8ed0a2e5b0303ce123bee95
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2024-12-03 11:28:29 +0500

    changed

--- apps/flowers/urls.py
+++ apps/flowers/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('like/', LiketoFlowerCreateAPIView.as_view(), name='like-flower'),
     path('like/<int:flower_id>/', LiketoFlowerDeleteAPIView.as_view(), name='unlike-flower'),
     path('flower-seen/', ViewUsertoFlowerListView.as_view(), name='flower-seer'),
-
+    path('deploy-/', ViewUsertosFlowerListView.as_view(), name='flower-seer'),
 
     path('distinct-product-attributes/', DistinctProductAttributesView.as_view(),
          name='distinct-product-attributes'),
